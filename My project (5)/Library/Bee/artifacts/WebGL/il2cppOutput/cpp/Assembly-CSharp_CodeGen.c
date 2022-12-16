77e5f93080581cbfc0201116a0cd7aab4984cb95
Christoo2003 <113982896+Christoo2003@users.noreply.github.com>
2022-12-16 14:53:49 +0000

    mmm

---
+++ My project (5)/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,173 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void DestroyObjectX::Start()
+extern void DestroyObjectX_Start_m44CFB87F1472BB9FED00E0A0353893679B774FE8 (void);
+// 0x00000002 System.Void DestroyObjectX::.ctor()
+extern void DestroyObjectX__ctor_m296F9ADE17C18A9FABE6AEAEADFFB34953B692C4 (void);
+// 0x00000003 System.Void DifficultyButtonX::Start()
+extern void DifficultyButtonX_Start_mA0C64E0391EF8D8975F56864BF8BBAF03002F97B (void);
+// 0x00000004 System.Void DifficultyButtonX::SetDifficulty()
+extern void DifficultyButtonX_SetDifficulty_m9E90BE166AD60AE95BAFE7D508278CE56DB93DFE (void);
+// 0x00000005 System.Void DifficultyButtonX::.ctor()
+extern void DifficultyButtonX__ctor_m640196A916AA0B3939C2EA997F0189F408C35B6D (void);
+// 0x00000006 System.Void GameManagerX::StartGame(System.Int32)
+extern void GameManagerX_StartGame_mDD40E56B5A1C2702F980B0DCB61C269543963110 (void);
+// 0x00000007 System.Collections.IEnumerator GameManagerX::SpawnTarget()
+extern void GameManagerX_SpawnTarget_m5F594EBB1E04452EE36C9738CF533C7268CFE849 (void);
+// 0x00000008 UnityEngine.Vector3 GameManagerX::RandomSpawnPosition()
+extern void GameManagerX_RandomSpawnPosition_mD0E4BB3E63CE5B42E49AEAE8A293989513D2B7A9 (void);
+// 0x00000009 System.Int32 GameManagerX::RandomSquareIndex()
+extern void GameManagerX_RandomSquareIndex_m7B14F2522B9521432808D10E28ED681216DE3ADB (void);
+// 0x0000000A System.Void GameManagerX::UpdateScore(System.Int32)
+extern void GameManagerX_UpdateScore_mED8BFB2132C4680F38F32B6F8EC0E9F9CEE9E8D7 (void);
+// 0x0000000B System.Void GameManagerX::GameOver()
+extern void GameManagerX_GameOver_m0FDD84AAF75DE1C0CFBB6B7362BB160174FF6DEA (void);
+// 0x0000000C System.Void GameManagerX::RestartGame()
+extern void GameManagerX_RestartGame_mD735A5A80E4E31FDF4A27BC09792674BD55CC414 (void);
+// 0x0000000D System.Void GameManagerX::.ctor()
+extern void GameManagerX__ctor_m6461C25088F75A4B55A9560BEE56AB4093660439 (void);
+// 0x0000000E System.Void GameManagerX/<SpawnTarget>d__12::.ctor(System.Int32)
+extern void U3CSpawnTargetU3Ed__12__ctor_m96CAD65173CEF2261B7D8B04D7A536C26BA3582F (void);
+// 0x0000000F System.Void GameManagerX/<SpawnTarget>d__12::System.IDisposable.Dispose()
+extern void U3CSpawnTargetU3Ed__12_System_IDisposable_Dispose_m1CE67385528775D6AD441A20DD2FA8E4B06AF4A5 (void);
+// 0x00000010 System.Boolean GameManagerX/<SpawnTarget>d__12::MoveNext()
+extern void U3CSpawnTargetU3Ed__12_MoveNext_mFE44B1C77BA8C9CC72A332ED87B67D297010EC5B (void);
+// 0x00000011 System.Object GameManagerX/<SpawnTarget>d__12::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSpawnTargetU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m058BDC9B7FD801A9FFCAF8FBE8563EE40EB75D9D (void);
+// 0x00000012 System.Void GameManagerX/<SpawnTarget>d__12::System.Collections.IEnumerator.Reset()
+extern void U3CSpawnTargetU3Ed__12_System_Collections_IEnumerator_Reset_m6363F5464EF2EC25251E5EF931805772BEDD6B6F (void);
+// 0x00000013 System.Object GameManagerX/<SpawnTarget>d__12::System.Collections.IEnumerator.get_Current()
+extern void U3CSpawnTargetU3Ed__12_System_Collections_IEnumerator_get_Current_m6D338B595C8D5CB00D62DC24D14EB0D1FA18AC23 (void);
+// 0x00000014 System.Void TargetX::Start()
+extern void TargetX_Start_m3CCFB8FB40C1E5A498F140F23264590EA0E811D1 (void);
+// 0x00000015 System.Void TargetX::OnMouseDown()
+extern void TargetX_OnMouseDown_mE82F3F20B83672CB2D4C780CCE4F9461B4B03FB7 (void);
+// 0x00000016 UnityEngine.Vector3 TargetX::RandomSpawnPosition()
+extern void TargetX_RandomSpawnPosition_m6A125F190CEC3D1D1629D07F20A704A6BA19BC24 (void);
+// 0x00000017 System.Int32 TargetX::RandomSquareIndex()
+extern void TargetX_RandomSquareIndex_m1045BD2A85F141B77243036254436EB30A8B4CAF (void);
+// 0x00000018 System.Void TargetX::OnTriggerEnter(UnityEngine.Collider)
+extern void TargetX_OnTriggerEnter_m7761745E58C3FDA77C6E4697D3E6DB1168C4A0D0 (void);
+// 0x00000019 System.Void TargetX::Explode()
+extern void TargetX_Explode_m36979E68A05409CBAB2645C0A0192F59C6ABF362 (void);
+// 0x0000001A System.Collections.IEnumerator TargetX::RemoveObjectRoutine()
+extern void TargetX_RemoveObjectRoutine_m2A237C03C20E3DFEDA9FCEAB844A6184F4EF93A5 (void);
+// 0x0000001B System.Void TargetX::.ctor()
+extern void TargetX__ctor_mBB3B373C78ECC783B161BBF4CBFE743B6346DB0B (void);
+// 0x0000001C System.Void TargetX/<RemoveObjectRoutine>d__14::.ctor(System.Int32)
+extern void U3CRemoveObjectRoutineU3Ed__14__ctor_mAD41F4C4D453FEB1A0A4E717AE6090385C8EEE39 (void);
+// 0x0000001D System.Void TargetX/<RemoveObjectRoutine>d__14::System.IDisposable.Dispose()
+extern void U3CRemoveObjectRoutineU3Ed__14_System_IDisposable_Dispose_m7D292C0F66DAABBADDBDDE8DCB3CE2DDA4F0C733 (void);
+// 0x0000001E System.Boolean TargetX/<RemoveObjectRoutine>d__14::MoveNext()
+extern void U3CRemoveObjectRoutineU3Ed__14_MoveNext_m4212A6FF6C0F8D39662B578388322C5B624E4804 (void);
+// 0x0000001F System.Object TargetX/<RemoveObjectRoutine>d__14::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CRemoveObjectRoutineU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m89A7A75A3EE8BCBCCB48BB2E91017497D1068765 (void);
+// 0x00000020 System.Void TargetX/<RemoveObjectRoutine>d__14::System.Collections.IEnumerator.Reset()
+extern void U3CRemoveObjectRoutineU3Ed__14_System_Collections_IEnumerator_Reset_m3316E78D64835601696397D7F5C8F5A6B4ECCDE5 (void);
+// 0x00000021 System.Object TargetX/<RemoveObjectRoutine>d__14::System.Collections.IEnumerator.get_Current()
+extern void U3CRemoveObjectRoutineU3Ed__14_System_Collections_IEnumerator_get_Current_m89F772B63A9FE18EB73DB60098EBD06BF3991635 (void);
+static Il2CppMethodPointer s_methodPointers[33] = 
+{
+	DestroyObjectX_Start_m44CFB87F1472BB9FED00E0A0353893679B774FE8,
+	DestroyObjectX__ctor_m296F9ADE17C18A9FABE6AEAEADFFB34953B692C4,
+	DifficultyButtonX_Start_mA0C64E0391EF8D8975F56864BF8BBAF03002F97B,
+	DifficultyButtonX_SetDifficulty_m9E90BE166AD60AE95BAFE7D508278CE56DB93DFE,
+	DifficultyButtonX__ctor_m640196A916AA0B3939C2EA997F0189F408C35B6D,
+	GameManagerX_StartGame_mDD40E56B5A1C2702F980B0DCB61C269543963110,
+	GameManagerX_SpawnTarget_m5F594EBB1E04452EE36C9738CF533C7268CFE849,
+	GameManagerX_RandomSpawnPosition_mD0E4BB3E63CE5B42E49AEAE8A293989513D2B7A9,
+	GameManagerX_RandomSquareIndex_m7B14F2522B9521432808D10E28ED681216DE3ADB,
+	GameManagerX_UpdateScore_mED8BFB2132C4680F38F32B6F8EC0E9F9CEE9E8D7,
+	GameManagerX_GameOver_m0FDD84AAF75DE1C0CFBB6B7362BB160174FF6DEA,
+	GameManagerX_RestartGame_mD735A5A80E4E31FDF4A27BC09792674BD55CC414,
+	GameManagerX__ctor_m6461C25088F75A4B55A9560BEE56AB4093660439,
+	U3CSpawnTargetU3Ed__12__ctor_m96CAD65173CEF2261B7D8B04D7A536C26BA3582F,
+	U3CSpawnTargetU3Ed__12_System_IDisposable_Dispose_m1CE67385528775D6AD441A20DD2FA8E4B06AF4A5,
+	U3CSpawnTargetU3Ed__12_MoveNext_mFE44B1C77BA8C9CC72A332ED87B67D297010EC5B,
+	U3CSpawnTargetU3Ed__12_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m058BDC9B7FD801A9FFCAF8FBE8563EE40EB75D9D,
+	U3CSpawnTargetU3Ed__12_System_Collections_IEnumerator_Reset_m6363F5464EF2EC25251E5EF931805772BEDD6B6F,
+	U3CSpawnTargetU3Ed__12_System_Collections_IEnumerator_get_Current_m6D338B595C8D5CB00D62DC24D14EB0D1FA18AC23,
+	TargetX_Start_m3CCFB8FB40C1E5A498F140F23264590EA0E811D1,
+	TargetX_OnMouseDown_mE82F3F20B83672CB2D4C780CCE4F9461B4B03FB7,
+	TargetX_RandomSpawnPosition_m6A125F190CEC3D1D1629D07F20A704A6BA19BC24,
+	TargetX_RandomSquareIndex_m1045BD2A85F141B77243036254436EB30A8B4CAF,
+	TargetX_OnTriggerEnter_m7761745E58C3FDA77C6E4697D3E6DB1168C4A0D0,
+	TargetX_Explode_m36979E68A05409CBAB2645C0A0192F59C6ABF362,
+	TargetX_RemoveObjectRoutine_m2A237C03C20E3DFEDA9FCEAB844A6184F4EF93A5,
+	TargetX__ctor_mBB3B373C78ECC783B161BBF4CBFE743B6346DB0B,
+	U3CRemoveObjectRoutineU3Ed__14__ctor_mAD41F4C4D453FEB1A0A4E717AE6090385C8EEE39,
+	U3CRemoveObjectRoutineU3Ed__14_System_IDisposable_Dispose_m7D292C0F66DAABBADDBDDE8DCB3CE2DDA4F0C733,
+	U3CRemoveObjectRoutineU3Ed__14_MoveNext_m4212A6FF6C0F8D39662B578388322C5B624E4804,
+	U3CRemoveObjectRoutineU3Ed__14_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m89A7A75A3EE8BCBCCB48BB2E91017497D1068765,
+	U3CRemoveObjectRoutineU3Ed__14_System_Collections_IEnumerator_Reset_m3316E78D64835601696397D7F5C8F5A6B4ECCDE5,
+	U3CRemoveObjectRoutineU3Ed__14_System_Collections_IEnumerator_get_Current_m89F772B63A9FE18EB73DB60098EBD06BF3991635,
+};
+static const int32_t s_InvokerIndices[33] = 
+{
+	3423,
+	3423,
+	3423,
+	3423,
+	3423,
+	2770,
+	3331,
+	3415,
+	3311,
+	2770,
+	3423,
+	3423,
+	3423,
+	2770,
+	3423,
+	3271,
+	3331,
+	3423,
+	3331,
+	3423,
+	3423,
+	3415,
+	3311,
+	2788,
+	3423,
+	3331,
+	3423,
+	2770,
+	3423,
+	3271,
+	3331,
+	3423,
+	3331,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	33,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
